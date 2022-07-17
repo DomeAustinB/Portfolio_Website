292df0388a1048d0232d16ca2cdffe28692ce01a
DomeAustinB <100167612+DomeAustinB@users.noreply.github.com>
2022-07-17 15:30:19 -0400

    adding projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,9 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -10,27 +13,12 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "The Admin Group",
+      description: "The social media app that helps you find new places to eat, go out, and enjoy life! All recommendations are based on a network of your friends and mutual friends' 'likes'.",
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
+      title: "The Admin Group",
       description: "Design & Development",
       imgUrl: projImg2,
     },
@@ -40,6 +28,24 @@ export const Projects = () => {
       imgUrl: projImg3,
     },
   ];
+
+  const projctS = [
+    {
+        title: "pocketAI",
+        description: "bus dev",
+        imgUrl: projImg4
+    },
+    {
+        title: "pocketAI",
+        description: "bus dev",
+        imgUrl: projImg5,
+    },
+    {
+        title: "pocketAI",
+        description: "bus dev",
+        imgUrl: projImg6,
+    },
+  ]
 
   return (
     <section className="project" id="project">
@@ -54,13 +60,13 @@ export const Projects = () => {
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      <Nav.Link eventKey="first">The Admin Group</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">pocketAI</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -78,11 +84,22 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
+                    <Tab.Pane eventKey="second">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <Row>
+                        {
+                          projctS.map((projctS, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...projctS}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
